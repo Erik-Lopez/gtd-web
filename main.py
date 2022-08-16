10071f8e6a7544d1bb4bfbf8438ab6eda531370f
Erik <eriklf442@gmail.com>
2022-08-16 20:13:56 +0200

    About me page.

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get(
-    path="/",
-    summary="Main page."
+    path = "/",
+    response_class = HTMLResponse,
+    summary = "Main page."
 )
 async def main_page():
-    return { "Hello": "World" }
+    with open("./static/main_page.html", "r") as f:
+        content = f.read()
+        return HTMLResponse(content)
+
+@app.get(
+    path = "/about",
+    response_class = HTMLResponse,
+    summary = "About me page."
+)
+async def about_me():
+    with open("./static/about.html", "r") as f:
+        content = f.read()
+        return HTMLResponse(content)
